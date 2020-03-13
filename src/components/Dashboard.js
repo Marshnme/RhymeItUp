f0bf754417708d493a97225a81e27d19cd898c3f
Joshua Holtsclaw <jildsmiles@yahoo.com>
2020-03-13 14:34:47 -0400

    basic component structure and route implemented. Need to find design to code

---
+++ src/components/Dashboard.js
@@ -0,0 +1,23 @@
+import React,{useState, useEffect} from "react";
+
+
+const Dashboard = () => {
+
+
+
+
+
+
+
+
+
+
+
+    return(
+        <div>
+            <h1>Do u wanna rhyme</h1>
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ src/components/Rhymes.js
@@ -0,0 +1,23 @@
+import React,{useState, useEffect} from "react";
+
+
+const Rhymes = () => {
+
+
+
+
+
+
+
+
+
+
+
+    return(
+        <div>
+            <h1>here is rhyme</h1>
+        </div>
+    )
+}
+
+export default Rhymes
