b53ef99d6bc3e6538774f145ecde172110273972
Joshua Holtsclaw <jildsmiles@yahoo.com>
2020-03-17 18:37:56 -0400

    resonse from api

--- src/components/Rhymes.js
+++ src/components/Rhymes.js
@@ -1,22 +1,40 @@
 import React,{useState, useEffect} from "react";
-
+import axios from "axios";
 
 const Rhymes = () => {
+    const [rhyme,setRhyme] = useState({word:""})
 
 
+    const handleChanges = e => {
+        setRhyme({[e.target.name]: e.target.value});
+        console.log(e.target.value)
+    };
 
-
-
-
-
-
-
-
-
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios
+        .get(`https://wordsapiv1.p.rapidapi.com/words/${rhyme.word}/rhymes`,{
+            headers:{"x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+            "x-rapidapi-key": "3e6f97ba46mshd0e970af8b190bcp1daa64jsn2a48b3562207"}
+        })
+        .then(res =>{
+            console.log("get res",res)
+        })
+        
+        
+        
+    }
+    console.log(rhyme)
     return(
-        <div>
-            <h1>here is rhyme</h1>
-        </div>
+        <form onSubmit={handleSubmit} className = "register-form">
+            <div>
+                <label>Find a rhyme for: </label>
+                <input onChange={handleChanges} value={rhyme.word} type="text" name="word" required></input>
+            </div>
+            <div>
+                <button>Submit!</button>
+            </div>
+        </form>
     )
 }
 
