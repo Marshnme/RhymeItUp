d31a788f2bffa804ad0c37f1be12fce51d038559
Joshua Holtsclaw <jildsmiles@yahoo.com>
2020-03-25 23:24:16 -0400

    basic flexbox and centering on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,7 @@
 import React,{useState, useEffect} from "react";
 import  {Link}  from "react-router-dom";
+import "./dashboard.css";
+
 
 const Dashboard = () => {
 
@@ -14,12 +16,12 @@ const Dashboard = () => {
 
 
     return(
-        <div>
+        <div className="parent">
             <header>
-                <h1>Bad Rhyme!</h1>
+                <h1 className="title">Bad Rhyme!</h1>
             </header>
-            <main>
-                <div>
+            <main className = "main">
+                <div className="greeting">
                     <p>At first I wished to create a beautiful rhyming website to help people write or just find better rhymes.</p>
                     <p>Then I realized the rhyming API I found was actually pretty terrible, so I decided to roll with it! </p>
                     <p>I hope you find laughter from these terrible rhymes as I did. Included is also a similiar word search that is completely hit or miss 😊</p>
